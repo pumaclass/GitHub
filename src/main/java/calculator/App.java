d577bea584786fada4034dcf84d83760544f2edb
giles <https://pumaclass@github.com>
2024-07-28 17:32:33 +0900

    2-7 과재완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -11,16 +11,17 @@ public class App {
         String restart;
         try {
             do { // while와 연동 될 do로 시작
+                System.out.println("1.사칙연산 수행하기(1입력), " + "2. 원의 넓이 구하기(2입력)");
+                int choice = sc.nextInt();
+                if(choice == 1){
                 System.out.print("첫 번째 숫자를 입력하세요: ");
                 int num1 = sc.nextInt(); // 인풋값을 받을 num1 생성
                 System.out.print("두 번째 숫자를 입력하세요: ");
                 int num2 = sc.nextInt(); // 인풋값을 받을 num2 생성
-
                 System.out.print("사칙 연산 기호를 입력하세요.");
                 char operator = sc.next().charAt(0); // 사칙연산 인풋을 받을 char 문자열 생성
 
                 double result = calculator.calculate(num1, num2, operator);
-
 //            switch (operator) { // switch 피연산자 변수 생성
 //                case '+':
 //                    result = (num1 + num2); // 덧셈 변수 생성
@@ -38,8 +39,7 @@ public class App {
 //                default: // 연산자에 일치하지 않는 값이 operator에 입력되면 에러 메세지 출력.
 //                    System.out.println("잘못된 연산입니다.");
 //                    continue;
-//            }
-
+//
                 System.out.println("계산결과 : " + result);
 //            for(int i = 0 ; i < intArray.length -1 ; i++){
 //                intArray[i] = intArray[i+1];
@@ -51,15 +51,13 @@ public class App {
 //                System.out.println(intArray[i]);
 
                 // }
-
-
                 System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
                 restart = sc.next();
-
                 if (restart.equals("remove")) {
                     System.out.println("제일 처음 계산한 값이 삭제되었습니다.");
                     calculator.removeFirstResult();
                 }
+
                 System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
                 restart = sc.next();
                 if (restart.equals("inquiry")) {
@@ -67,13 +65,19 @@ public class App {
                     for (Double data : results) {
                         System.out.println(data);
                     }
+                    }
+                }else if(choice==2){
+                    System.out.println("반지름의 넓이를 입력하세요.");
+                    Calculator.Circle.radius = sc.nextInt();
+                    System.out.println("반지름 " + Calculator.Circle.radius);
+                    System.out.println("원의 지름" + Calculator.Circle.getArea());
                 }
 
                 System.out.println("더 계산하시겠습니까? " + "(restart 입력시 재시작, exit 입력시 프로그램 종료.)");
                 restart = sc.next(); // 미리 생성 된 문자열에 종료 관련 인풋값 출력
             } while (!restart.equals("exit")); // 반복문 종료 및 exit를 작성하면 프로그램이 종료되는 행동 정의
         } catch (ArithmeticException | IllegalArgumentException e){
-            System.out.println("연산에러 발생" + e.getMessage());
+            System.out.println("연산에러 발생 : " + e.getMessage());
         }finally {
             sc.close();
         }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,16 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-    private List<Double> results;
+    private List<Double> results; // 결과(배열) 조회 매서드
 
     public Calculator() {
-        this.results = new ArrayList<>();
+        this.results = new ArrayList<>(); // 결과(배열) 초기화 매서드
     }
-
     public double calculate(int num1, int num2, char operator) throws ArithmeticException, IllegalArgumentException {
         double result = 0;
 
-        switch (operator) {
+        switch (operator) { // 사칙연산 기능 구형
             case '+':
                 result = num1 + num2;
                 break;
@@ -28,23 +27,45 @@ public class Calculator {
 
             case '/':
                 if (num2 == 0) {
-                    throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                    throw new ArithmeticException("0으로 나눌 수 없습니다."); // 0으로 나눌경우 예외처리
                 }
                 result = (double) num1 / num2;
                 break;
             default:
-                throw new IllegalArgumentException("연산자가 잘못되었습니다." + operator);
+                throw new IllegalArgumentException("연산자가 잘못되었습니다." + operator); // 그외 연산 예와처리
         }
-        results.add(result);
+        results.add(result); // 결과값 Array에 할당
         return result;
     }
-    public void removeFirstResult(){
+    public void removeFirstResult(){ // 결과에서 첫번째 값 삭제 매서드
         if(!results.isEmpty()){
             results.remove(0);
 
         }
     }
-    public List<Double> getResults() {
+    public List<Double> getResults() { // 배열 조회 매서드
         return results;
+
+
+    }
+    public void setResults(List<Double>results){
+        this.results=results;
+    }
+
+    public class Circle{
+        static double radius;
+        public final static double PI = 3.14159;
+        public Circle(double radius){
+            this.radius = radius;
+        }
+        public double getRadius(double radius){
+            return radius;
+        }
+        public void setRadius(double radius){
+            this.radius = radius;
+        }
+        public static double getArea(){
+            return PI*radius*radius;
+        }
     }
 }
